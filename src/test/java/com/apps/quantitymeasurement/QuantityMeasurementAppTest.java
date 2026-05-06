4e99658aa1c178faa08b6a5dc8ddcde46b21117c
Arijit065 <arijitmandal065@gmail.com>
2026-05-07 00:21:57 +0530

    Implemented UC11 Volume Measurement

--- src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
+++ src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
@@ -1,44 +1,100 @@
 @Test
-void shouldReturnTrueForFeetAndInchEquality() {
+void shouldReturnTrueForLitreAndMillilitreEquality() {
 
-    Quantity<LengthUnit> feet =
-            new Quantity<>(1.0, LengthUnit.FEET);
+    Quantity<VolumeUnit> litre =
+            new Quantity<>(1.0, VolumeUnit.LITRE);
 
-    Quantity<LengthUnit> inch =
-            new Quantity<>(12.0, LengthUnit.INCH);
+    Quantity<VolumeUnit> milliLitre =
+            new Quantity<>(1000.0, VolumeUnit.MILLILITRE);
 
-    assertEquals(feet, inch);
+    assertEquals(litre, milliLitre);
 }
 
 @Test
-void shouldConvertKgToGram() {
+void shouldConvertLitreToMillilitre() {
 
-    Quantity<WeightUnit> kg =
-            new Quantity<>(1.0, WeightUnit.KG);
+    Quantity<VolumeUnit> litre =
+            new Quantity<>(1.0, VolumeUnit.LITRE);
 
-    Quantity<WeightUnit> result =
-            kg.convertTo(WeightUnit.G);
+    Quantity<VolumeUnit> result =
+            litre.convertTo(VolumeUnit.MILLILITRE);
 
     assertEquals(
-            new Quantity<>(1000.0, WeightUnit.G),
+            new Quantity<>(1000.0,
+                    VolumeUnit.MILLILITRE),
             result
     );
 }
 
 @Test
-void shouldAddWeights() {
+void shouldConvertGallonToLitre() {
 
-    Quantity<WeightUnit> kg =
-            new Quantity<>(1.0, WeightUnit.KG);
+    Quantity<VolumeUnit> gallon =
+            new Quantity<>(1.0, VolumeUnit.GALLON);
 
-    Quantity<WeightUnit> gram =
-            new Quantity<>(1000.0, WeightUnit.G);
-
-    Quantity<WeightUnit> result =
-            kg.add(gram);
+    Quantity<VolumeUnit> result =
+            gallon.convertTo(VolumeUnit.LITRE);
 
     assertEquals(
-            new Quantity<>(2.0, WeightUnit.KG),
+            new Quantity<>(3.79,
+                    VolumeUnit.LITRE),
             result
     );
+}
+
+@Test
+void shouldAddLitreAndMillilitre() {
+
+    Quantity<VolumeUnit> litre =
+            new Quantity<>(1.0, VolumeUnit.LITRE);
+
+    Quantity<VolumeUnit> milliLitre =
+            new Quantity<>(1000.0,
+                    VolumeUnit.MILLILITRE);
+
+    Quantity<VolumeUnit> result =
+            litre.add(milliLitre);
+
+    assertEquals(
+            new Quantity<>(2.0,
+                    VolumeUnit.LITRE),
+            result
+    );
+}
+
+@Test
+void shouldAddVolumesInTargetUnit() {
+
+    Quantity<VolumeUnit> litre =
+            new Quantity<>(1.0, VolumeUnit.LITRE);
+
+    Quantity<VolumeUnit> milliLitre =
+            new Quantity<>(1000.0,
+                    VolumeUnit.MILLILITRE);
+
+    Quantity<VolumeUnit> result =
+            litre.add(
+                    milliLitre,
+                    VolumeUnit.MILLILITRE
+            );
+
+    assertEquals(
+            new Quantity<>(2000.0,
+                    VolumeUnit.MILLILITRE),
+            result
+    );
+}
+
+@Test
+void shouldReturnFalseForDifferentCategories() {
+
+    Quantity<LengthUnit> length =
+            new Quantity<>(1.0,
+                    LengthUnit.FEET);
+
+    Quantity<VolumeUnit> volume =
+            new Quantity<>(1.0,
+                    VolumeUnit.LITRE);
+
+    assertNotEquals(length, volume);
 }
